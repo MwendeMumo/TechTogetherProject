2901d75107aeae747d7d52208fc827f2152bd5f5
Analyst Tammy <analysttammy@gmail.com>
2021-09-11 17:32:58 -0500

    created additional pages

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    Company_Name = models.CharField(max_length=200)
+    InterestRateAfterDefault = models.FloatField()
+    InitialLoanAmount = models.FloatField()
+    LoanRateAfterDefault = models.FloatField()
+    OverAllRating = models.FloatField()
+    FinalAmount = models.FloatField()
+    
--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . import models
 
-def homePageView(request):
-    return HttpResponse('Hello, World!')
 
+from django.views import generic
+from django.views.generic.edit import CreateView
+
+class HomePageView(generic.TemplateView):
+    template_name="home.html"
+
+# def index(response, id):
+#         ls = ToDoList.objects.get(id=id)
+#         return render(response,"main/list.html",{"ls":ls})
+
+# def home(response):
+#     return render(response, "main/home.html",{})
+
+class create(CreateView): 
+    model = models.User
+    template_name = "main/create.html"
+    fields = [ "Company_Name", "InterestRateAfterDefault"]
+  
