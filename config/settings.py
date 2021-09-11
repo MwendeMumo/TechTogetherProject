3614381f40ea987b70b62e9ac169293730b7ad9c
Analyst Tammy <analysttammy@gmail.com>
2021-09-11 17:32:58 -0500

    created additional pages

--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6+b&5vabz1_++r16p2*g26vz-215x7_$9d_y_#(8%p37(qn=s%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'pages',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +54,9 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            str(BASE_DIR.joinpath('templates/pages'))
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path
-from .views import homePageView
+from django.urls import path, include
+from .views import HomePageView
+from . import views
 
 urlpatterns = [
-    path('', homePageView, name='home')
+   #path("<int:id>", views.index, name="index"),
+    path('', HomePageView.as_view(), name='home'),
+    path("create/", views.create.as_view(), name='create'),
 ]
