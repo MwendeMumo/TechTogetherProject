6bb6744c6a79c5ad2b3c7852301647daf47c9351
Analyst Tammy <analysttammy@gmail.com>
2021-09-12 01:00:53 -0500

    added in lists and dataviz

--- pages/models.py
+++ pages/models.py
@@ -2,9 +2,12 @@ from django.db import models
 
 class User(models.Model):
     Company_Name = models.CharField(max_length=200)
-    InterestRateAfterDefault = models.FloatField()
-    InitialLoanAmount = models.FloatField()
-    LoanRateAfterDefault = models.FloatField()
-    OverAllRating = models.FloatField()
-    FinalAmount = models.FloatField()
-    
+    Initial_Loan_Amount = models.FloatField()
+    Initial_Interest_Rate = models.FloatField()
+    Loan_Rate_After_Default = models.FloatField()
+    Loan_Duration = models.FloatField()
+    Final_Loan_Amount = models.FloatField()
+    Overall_Rating = models.FloatField()
+
+    def get_absolute_url(self):
+        return u'/list/' 
--- pages/urls.py
+++ pages/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path, include
-from .views import HomePageView
+from .views import HomePageView, ListView, DataVizView
 from . import views
+from django.contrib import admin
 
 urlpatterns = [
    #path("<int:id>", views.index, name="index"),
     path('', HomePageView.as_view(), name='home'),
     path("create/", views.create.as_view(), name='create'),
+    path('list/', ListView.as_view(), name='list'),
+    path('dataviz/', DataVizView.as_view(), name='dataviz'),
+    path('admin/',admin.site.urls)
 ]
